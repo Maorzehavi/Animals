bbab88a68c4bbae1ad1ce7f5475a9d3e46638a54
Maor Zehavi <103786336+Maorzehavi@users.noreply.github.com>
2022-07-12 01:39:40 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,7 @@ import animals.seaAnimals.Whale;
 
 public class Main {
     public static void main(String[] args) {
-        Whale whale = new Whale("whale",60);
-
-        System.out.println(whale.getCurrentCapacity());
+        
 
     }
 }
